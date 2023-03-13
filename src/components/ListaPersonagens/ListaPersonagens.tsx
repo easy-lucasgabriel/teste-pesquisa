a0f10da8dfdf837e4407c9ea72ab05c3afcfbd4d
Pedro Neto <pedro.silveira@easychange.com.br>
2023-03-13 18:02:53 -0300

    Ordenando lista

--- src/components/ListaPersonagens/ListaPersonagens.tsx
+++ src/components/ListaPersonagens/ListaPersonagens.tsx
@@ -1,20 +1,49 @@
+import { useState } from "react";
+import { Div } from "components";
+import { Resultados } from "interfaces";
 
-import { Resultados } from 'interfaces';
+interface ResultadosProps {
+  resultados: Resultados[];
+}
 
- interface ResultadosProps {
-    resultados: Resultados[];
- }
- 
+function ListaPersonagens({ resultados }: ResultadosProps) {
+  const [order, setOrder] = useState(1);
+  const [columnOrder, setColumnOrder] = useState("");
 
-function ListaPersonagens({ resultados }:ResultadosProps) {
+  const handleOrder = (fieldName: any) => {
+    setOrder(-order);
+    setColumnOrder(fieldName);
 
-    return (
-        <ul>
-      {resultados.map((personagem,index) => (
-        <li key={index}>{personagem.name}</li>
-      ))}
+    resultados.sort((a: any, b: any) => {
+      return a[columnOrder] < b[columnOrder] ? -order : order;
+    });
+  };
+
+  return (
+    <ul>
+      <li onClick={(e) => handleOrder("birth_year")}>Aperta aqui niver</li>
+      <li onClick={(e) => handleOrder("name")}>Aperta aqui name</li>
+      <li onClick={(e) => handleOrder("height")}>Aperta aqui altura</li>
+      {resultados.map((personagem, index) => {
+        return (
+          <Div key={index} justifyContent="space-evenly">
+              
+            <div>
+              {personagem.name}
+            </div>
+              
+            <div>
+              {personagem.height}
+            </div>
+              
+            <div>
+              {personagem.birth_year}
+            </div>
+          </Div>
+        );
+      })}
     </ul>
-    )
+  );
 }
 
 export default ListaPersonagens;
