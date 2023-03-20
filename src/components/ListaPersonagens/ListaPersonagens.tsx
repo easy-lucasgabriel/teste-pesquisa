ae605bd196fc2a7856509fc76857337e450464fa
Pedro Neto <pedro.silveira@easychange.com.br>
2023-03-20 09:48:42 -0300

    Estilizando tabela de resultados

--- src/components/ListaPersonagens/ListaPersonagens.tsx
+++ src/components/ListaPersonagens/ListaPersonagens.tsx
@@ -3,47 +3,63 @@ import { RootObject } from 'interfaces';
 import dayjs from 'dayjs';
 import { Column, Div } from 'components';
 
- interface ResultadosProps {
-    resultados: RootObject[];
- }
- 
-export function ListaPersonagens({ resultados }:ResultadosProps) {
+interface ResultadosProps {
+  resultados: RootObject[];
+}
 
-    return (
-        <Column
+export function ListaPersonagens({ resultados }: ResultadosProps) {
+
+  console.log(resultados);
+
+  return (
+    <Column
+      width="100%"
+      flexDirection="column"
+    >
+      <Div
         width="100%"
-        >
-          <Div
-          width="100%"
-          >
-          <Div
+        height="4vh"
+      // bg="#00000056"
+      >
+        <Div
           minWidth="40%"
-          >Email</Div>
-          <Div
+        >Email</Div>
+        <Div
           minWidth="20%"
-          >Valor total apostado</Div>
-          <Div
+        >Valor total apostado</Div>
+        <Div
           minWidth="20%"
-          >Data da aposta</Div>
+        >Data da aposta</Div>
+      </Div>
+
+      {resultados.map((personagem, index) => (
+        <Column
+          key={index}>
+          <Div
+            minWidth="40%">
+            {personagem.data_content.sending.username}
           </Div>
 
-      {resultados.map((personagem,index) => (
-        <Column 
-        key={index}>
-          <Div>
-            {personagem.data_content.sending.username}
-            </Div>
-
-          <Div>
+          <Div
+            minWidth="20%"
+          >
             {personagem.data_content.sending.value} R$
-            </Div>
+          </Div>
 
-          <Div>
+          <Div
+            minWidth="20%"
+          >
             {dayjs(personagem.modified_date).format("DD/MM/YYYY HH:mm:ss")}
-            </Div>
+          </Div>
+
+          <Div
+            minWidth="20%"
+          >
+            {personagem.data_content.return.Message}
+          </Div>
 
         </Column>
       ))}
     </Column>
-    )
+  )
 }
